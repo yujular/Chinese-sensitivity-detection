5f0638ef718f43e1b02a1c38b2346f6ce9b8aa1a
yujular <22210720287@m.fudan.edu.cn>
2023-11-28 15:28:12 +0800

    debug

--- test.py
+++ test.py
@@ -29,18 +29,20 @@ if __name__ == '__main__':
     model = Model(args).get_model()
 
     model_path = os.path.join(args.train['model_out_path'],
+                              'class-' + str(args.dataset['class_num']),
                               args.model['model_name'] + 'model.bin')
     print("Loading model from {}...".format(model_path))
     model = load_torch_model(model=model, model_path=model_path, device=args.train['device'], strict=True)
 
     # 开发集预测
     # ans, label = get_prediction(model, dev_loader, args.train['device'], test=False)
-    # acc, f1 = calculate_accuracy_f1(label, ans, class_num=2)
+    # acc, f1 = calculate_accuracy_f1(label, ans, class_num=args.dataset['class_num'])
     # print("Dev acc: {}, f1: {}".format(acc, f1))
 
     # 测试集预测
     ans, label, fine_grained_label = get_prediction(model, test_loader, args.train['device'], test=True)
-    test_evaluate = evaluate_subcategory(label, ans, fine_grained_label, class_num=2, average='macro')
+    test_evaluate = evaluate_subcategory(label, ans, fine_grained_label, class_num=args.dataset['class_num'],
+                                         average='macro')
     print("Test acc: {}, f1: {}, acc_I: {}, f1_I: {}, acc_G: {}, f1_G: {}, acc_anti: {}, f1_anti: {}, acc_non_offen: "
           "{}, f1_non_offen: {}".format(*test_evaluate)
           )
