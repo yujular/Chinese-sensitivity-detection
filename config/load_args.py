429d551571d6fc4d2dd29bc8b3fb1f5b86c1a616
yujular <22210720287@m.fudan.edu.cn>
2023-12-08 12:12:53 +0800

    feat: using configargparse

--- config/load_args.py
+++ config/load_args.py
@@ -1,7 +1,8 @@
-import argparse
-
+import configargparse
 import torch.cuda
 import yaml
+
+from utils import str2bool
 
 
 def load_args(config_file):
@@ -30,9 +31,63 @@ def load_args(config_file):
     return parser.parse_args()
 
 
+def add_input_args(parser):
+    # global config
+    parser.add_argument('--seed', type=int, default=3407, help='Random seed.')
+    parser.add_argument('--num_workers', type=int, default=0)
+
+    # model config
+    parser.add_argument('--transfer', type=str2bool, default=False, help='Whether to use transfer learning.')
+    parser.add_argument('--pretrained', type=str2bool, default=False, help='Whether to use pretrained model.')
+    parser.add_argument('--model_path', type=str, default='models', help='Model root path.')
+    parser.add_argument('--model', '-m', type=str, default='bert-base-chinese', help='backbone network')
+    parser.add_argument('--cls', '-c', type=str, default='linear', help='classifier')
+    parser.add_argument('--dropout_rate', type=float, default=0.1, help='dropout rate')
+    parser.add_argument('--hugging_face', type=str2bool, default=False,
+                        help='whether to use hugging face to load models')
+
+    # data config
+    parser.add_argument('--data_dir', type=str, default='data', help='Data root path.')
+    parser.add_argument('--train_data', type=str, default='COLD', help='Training dataset name.')
+    parser.add_argument('--source_data', type=str, default='KOLD', help='Source dataset name.')
+
+    # train config
+    parser.add_argument('--epochs', type=int, default=20, help='Number of epochs to train.')
+    parser.add_argument('--batch_size', type=int, default=32, help='Number of batch size to train.')
+    parser.add_argument('--freeze', type=str2bool, default=False, help='Freeze the encoder weights.')
+    parser.add_argument('--clip_grad', type=str2bool, default=False, help='Clip the gradient norm.')
+    parser.add_argument('--max_grad_norm', type=float, default=1.0e-3, help='Max gradient norm.')
+
+    # optimizer config
+    parser.add_argument('--optimizer', '-o', type=str, default='AdamW', help='Optimizer.')
+    parser.add_argument('--lr', type=float, default=5.0e-5, help='Initial learning rate.')
+
+    # scheduler config
+    parser.add_argument('--scheduler', '-s', type=str, default='warmuplinear', help='Scheduler.')
+    parser.add_argument('--warmup_steps', type=int, default=500, help='Warm up steps.')
+
+
+def get_parser():
+    parser = configargparse.ArgumentParser(
+        description="Transfer learning for OLD config parser",
+        config_file_parser_class=configargparse.YAMLConfigFileParser,
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
+    )
+    # get parser from config file
+    # priority: command line > config file > default
+    parser.add_argument("--config", is_config_file=True, help="config file path")
+    add_input_args(parser)
+
+    parser_args = parser.parse_args()
+    # device setting
+    setattr(parser_args, 'device', 'cuda' if torch.cuda.is_available() else 'cpu')
+    setattr(parser_args, 'n_gpu', torch.cuda.device_count())
+    return parser_args
+
+
 if __name__ == "__main__":
     # 传入config.yml文件的路径作为参数
-    args = load_args("config.yml")
+    args = get_parser()
 
     # 使用args中的参数进行你的操作
-    print(args.model['model_name'])
+    print(args)
