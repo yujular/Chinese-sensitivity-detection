bd8883a085acba0dfb046fc20d9083d0b7082c27
yujular <22210720287@m.fudan.edu.cn>
2023-12-12 11:40:08 +0800

    chore: update init

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,6 +3,6 @@ from . import OLID
 from . import load_data
 
 load_dataset = load_data.load_dataset
-get_dataloader = load_data.get_dataloaders
+get_dataloaders = load_data.get_dataloaders
 COLDataset = COLD.COLDataset
 OLIDataset = OLID.OLIDataset
